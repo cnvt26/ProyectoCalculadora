a39408a1ade931750ff8b158388a4ffb067df9d7
cnvt26 <cnvt26@gmail.com>
2024-02-16 00:47:47 +0100

    Agregada clase de prueba para la clase Calculadora

---
+++ pruebaCalculadora.java
@@ -0,0 +1,8 @@
+package ProyectoCalculadora;
+
+public class pruebaCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora(10, 5);
+        System.out.println("La suma es: " + calc.suma());
+    }
+}
