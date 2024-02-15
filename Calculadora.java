b23e8c976e812c1fc4f05479badcfc72e536add3
cnvt26 <cnvt26@gmail.com>
2024-02-16 00:46:32 +0100

    Commit Inicial - Implementación del método suma en la clase Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+package ProyectoCalculadora;
+
+public class Calculadora {
+    private int numero1;
+    private int numero2;
+
+    public Calculadora(int num1, int num2) {
+        this.numero1 = num1;
+        this.numero2 = num2;
+    }
+
+    public int suma() {
+        return this.numero1 + this.numero2;
+    }
+}
